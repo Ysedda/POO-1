696c7561eb15e1c2072ae429d4ba1ec684640635
Ysedda <86974668+Ysedda@users.noreply.github.com>
2022-03-28 11:30:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,9 @@
 requireonce 'Car.php';
 requireonce 'Bicycle';
 
-$ferrari = new Car(red, 2, gpl);
+$ferrari = new Car('red', 2, 'gpl');
+var_dump($ferrari);
+$ferrari->forward();
+var_dump($ferrari);
+$ferrari->brake();
 var_dump($ferrari);
